803ec0c5a7ecfd74447ec0ec7c820d41029fe85a
developer shrey <81847438+shrey-coder@users.noreply.github.com>
2022-10-17 17:20:13 +0530

    student changes

--- js/validating_register.js
+++ js/validating_register.js
@@ -3,7 +3,8 @@ const register_password = document.getElementById("register_password");
 const phone = document.getElementById("phone");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
-const register_email_regex = /^[^@$%&_0-9]{1,}\@(gmail|yahoo)(.com|.in|.co.in)$/;
+const register_email_regex =
+  /^[^@$%&_0-9][0-9a-zA-Z]{1,}@(gmail|yahoo)(.com|.in|.co.in)$/;
 const phone_regex = /^[6-9][0-9]{9}$/;
 register_email.addEventListener("blur", () => {
   const check = register_email.value.match(register_email_regex);
