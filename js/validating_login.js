4ba21ae9e127801693d3e2375d139de7d646c440
priyanshshah2442 <108476017+priyanshshah2442@users.noreply.github.com>
2022-10-17 16:40:40 +0530

    login

--- js/validating_login.js
+++ js/validating_login.js
@@ -1,18 +1,18 @@
-const login_email = document.getElementById("login_email");
+// const login_email = document.getElementById("login_email");
 
-// const password_regex = /{6,}/;
-const login_email_regex = /^[^@$%&_0-9]{1,}\@(gmail|yahoo)(.com|.in|.co.in)$/;
+// // const password_regex = /{6,}/;
+// const login_email_regex = /^[^@$%&_0-9]{1,}\@(gmail|yahoo)(.com|.in|.co.in)$/;
 
-login_email.addEventListener("blur", () => {
-  const check = login_email.value.match(login_email_regex);
-  if (!check) {
-    alert("Enter a valid email");
-    login_email.value = "";
-  }
-});
-login_password.addEventListener("blur", () => {
-  if (login_password.value.length < 6) {
-    alert("Password length should be atleast 6 characters");
-    login_password.value = "";
-  }
-});
+// login_email.addEventListener("blur", () => {
+//   const check = login_email.value.match(login_email_regex);
+//   if (!check) {
+//     alert("Enter a valid email");
+//     login_email.value = "";
+//   }
+// });
+// login_password.addEventListener("blur", () => {
+//   if (login_password.value.length < 6) {
+//     alert("Password length should be atleast 6 characters");
+//     login_password.value = "";
+//   }
+// });
