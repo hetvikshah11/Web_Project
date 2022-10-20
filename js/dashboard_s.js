4e966b3025917884c21bdb3d83354cdf9bd68ee5
Hetvik Shah <hetvikshah11@gmail.com>
2022-10-20 13:43:07 +0530

    1.43 pm

--- js/dashboard_s.js
+++ js/dashboard_s.js
@@ -1,15 +1,3 @@
-function change() {
-    var input = document.getElementById("filechooser");
-    var fReader = new FileReader();
-    fReader.readAsDataURL(input.files[0]);
-    fReader.onloadend = function (event) {
-        var img = document.getElementById("profilepic");
-        img.src = event.target.result;
-        img.style.width = '150px'
-        img.style.height = '150px'
-        img.style.display = 'block'
-    }
-}
 document.getElementById('filechooser').onmouseover = function(){
     document.getElementById('blackfill').style.opacity = '0.3'
 }
--- js/dashboard_t.js
+++ js/dashboard_t.js
@@ -1,27 +1,48 @@
-function change() {
-    var input = document.getElementById("filechooser");
-    var fReader = new FileReader();
-    fReader.readAsDataURL(input.files[0]);
-    fReader.onloadend = function (event) {
-        var img = document.getElementById("profilepic");
-        img.src = event.target.result;
-        img.style.width = '150px'
-        img.style.height = '150px'
-        img.style.display = 'block'
+function menu_click() {
+    var element = event.target;
+    home.classList.remove("active");
+    courses.classList.remove("active");
+    grades.classList.remove("active");
+    tasks.classList.remove("active");
+    calender.classList.remove("active");
+    inbox.classList.remove("active");
+    settings.classList.remove("active");
+    home_content.style.display = "none";
+    attendance_content.style.display = "none";
+    notes_content.style.display = "none";
+    grades_content.style.display = "none";
+    settings_content.style.display = "none";
+    if (element.id == home.id) {
+        home.classList.add("active");
+        home_content.style.display = "block";
+    }
+    else if (element.id == attendance.id) {
+        attendance.classList.add("active");
+        attendance_content.style.display = "block";
+    }
+    else if (element.id == notes.id) {
+        notes.classList.add("active");
+        notes_content.style.display = "block";
+    }
+    else if (element.id == grades.id) {
+        grades.classList.add("active");
+        grades_content.style.display = "block";
+    }
+    else if (element.id == settings.id) {
+        settings.classList.add("active");
+        settings_content.style.display = "block";
     }
 }
-document.getElementById('filechooser').onmouseover = function(){
+
+
+document.getElementById('filechooser').onmouseover = function () {
     document.getElementById('blackfill').style.opacity = '0.3'
 }
-document.getElementById('filechooser').onmouseout= function(){
+document.getElementById('filechooser').onmouseout = function () {
     document.getElementById('blackfill').style.opacity = '0'
 }
 
 let rdata = null;
 rdata = JSON.parse(document.getElementById('received_data').innerHTML);
 console.log(rdata)
-document.getElementById('welcome').innerHTML = 'Welcome '+rdata.First_name;
-
-document.getElementById('teacher_logout').onclick = function(){
-    open('./views/index.html');
-}
+document.getElementById('welcome').innerHTML = 'Welcome ' + rdata.First_name;
