f8100a3ba9d161c7f6197c514c48714006b24da8
Hetvik Shah <hetvikshah11@gmail.com>
2022-10-20 15:22:00 +0530

    Co-authored-by: priyanshshah2442 <priyanshshah2442@users.noreply.github.com>

--- js/dashboard_t.js
+++ js/dashboard_t.js
@@ -1,11 +1,21 @@
+let home = document.getElementById('home')
+let grades = document.getElementById('grades')
+let attendance = document.getElementById('attendance')
+let notes = document.getElementById('notes')
+let settings = document.getElementById('settings')
+
+let home_content = document.getElementById('home_content')
+let attendance_content = document.getElementById('attendance_content')
+let notes_content = document.getElementById('notes_content')
+let grades_content = document.getElementById('grades_content')
+let settings_content = document.getElementById('settings_content')
+
 function menu_click() {
     var element = event.target;
     home.classList.remove("active");
-    courses.classList.remove("active");
     grades.classList.remove("active");
-    tasks.classList.remove("active");
-    calender.classList.remove("active");
-    inbox.classList.remove("active");
+    attendance.classList.remove("active");
+    notes.classList.remove("active");
     settings.classList.remove("active");
     home_content.style.display = "none";
     attendance_content.style.display = "none";
@@ -14,23 +24,23 @@ function menu_click() {
     settings_content.style.display = "none";
     if (element.id == home.id) {
         home.classList.add("active");
-        home_content.style.display = "block";
+        home_content.style.display = "flex";
     }
     else if (element.id == attendance.id) {
         attendance.classList.add("active");
-        attendance_content.style.display = "block";
+        attendance_content.style.display = "flex";
     }
     else if (element.id == notes.id) {
         notes.classList.add("active");
-        notes_content.style.display = "block";
+        notes_content.style.display = "flex";
     }
     else if (element.id == grades.id) {
         grades.classList.add("active");
-        grades_content.style.display = "block";
+        grades_content.style.display = "flex";
     }
     else if (element.id == settings.id) {
         settings.classList.add("active");
-        settings_content.style.display = "block";
+        settings_content.style.display = "flex";
     }
 }
 
