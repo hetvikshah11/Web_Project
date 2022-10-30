474e2cb5420a4356f2d6efcde9481cc4c679a7a1
Hetvik Shah <hetvikshah11@gmail.com>
2022-10-30 15:13:53 +0530

    3.13

--- js/dashboard_s.js
+++ js/dashboard_s.js
@@ -4,15 +4,13 @@ function menu_item() {
     courses.classList.remove("active");
     grades.classList.remove("active");
     tasks.classList.remove("active");
-    calender.classList.remove("active");
-    inbox.classList.remove("active");
+    materials.classList.remove("active");
     settings.classList.remove("active");
     home_content.style.display = "none";
     courses_content.style.display = "none";
     grades_content.style.display = "none";
     tasks_content.style.display = "none";
-    calender_content.style.display = "none";
-    inbox_content.style.display = "none";
+    materials_content.style.display = "none";
     settings_content.style.display = "none";
     if (element.id == home.id) {
       home.classList.add("active");
@@ -26,12 +24,9 @@ function menu_item() {
     } else if (element.id == tasks.id) {
       tasks.classList.add("active");
       tasks_content.style.display = "block";
-    } else if (element.id == calender.id) {
-      calender.classList.add("active");
-      calender_content.style.display = "block";
-    } else if (element.id == inbox.id) {
-      inbox.classList.add("active");
-      inbox_content.style.display = "block";
+    } else if (element.id == materials.id) {
+      materials.classList.add("active");
+      materials_content.style.display = "block";
     } else if (element.id == settings.id) {
       settings.classList.add("active");
       settings_content.style.display = "block";
